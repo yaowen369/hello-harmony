4153c537df9899e84c53820e3fad9c962850181e
yaowen <yaowen@bwton.com>
2025-10-22 20:10:26 +0800

    动态修改metadata值，功能充分ok

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,38 +1,10 @@
-import { hapTasks, OhosHapContext, OhosPluginId  } from '@ohos/hvigor-ohos-plugin';
+import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 import { getNode } from '@ohos/hvigor'
-import { hvigor } from '@ohos/hvigor';
-
-const debug_getui_appid = '1111'
-const KEY_GETUI_APPID = 'metadata_key'
+import { CompileHook } from './src/utils/CompileHook';
 
 const entryNode = getNode(__filename);
-entryNode.afterNodeEvaluate(node => {
-
-  const appContext = hvigor.getRootNode().getContext(OhosPluginId.OHOS_APP_PLUGIN) as OhosAppContext;
-  const buildMode = appContext.getBuildMode();
-
-  // 获取此节点使用插件的上下文对象 此时为hap插件 获取hap插件上下文对象
-  const hapContext = node.getContext(OhosPluginId.OHOS_HAP_PLUGIN) as OhosHapContext;
-  // 通过上下文对象获取从module.json5文件中读出来的obj对象
-  const moduleJsonOpt = hapContext.getModuleJsonOpt();
-  
-  // 修改obj对象为想要的，此处修改module中的metadata
-  const originalMetadata = moduleJsonOpt['module']['metadata'];
-  console.log(`[八维通编译hook] 编译类型:${buildMode}, 原始metadata:${JSON.stringify(originalMetadata)}, ` );
-
-  // 查找并修改metadata_key的值
-  let metadataItem = moduleJsonOpt['module']['metadata'].find(item => item.name === KEY_GETUI_APPID);
-  if ((buildMode === 'debug' || buildMode === 'Default') && metadataItem) {
-    console.log(`[八维通编译hook] 找到metadata_key，原始值: ${metadataItem.value}, 为修改${debug_getui_appid}`);
-    metadataItem.value = debug_getui_appid;
-  } else {
-    console.log(`[八维通编译hook] 当前编译类型为release，或 没有检测到 metadata_key, 不修改, 编译类型: ${buildMode}`);
-  }
-  
-  console.log('[八维通编译hook] 修改后的metadata:', moduleJsonOpt['module']['metadata']);
-
-  hapContext.setModuleJsonOpt(moduleJsonOpt);
-})
+// 设置编译时的 metadata 修改 hook
+CompileHook.setupMetadataHook(entryNode);
 
 export default {
   system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
---
+++ entry/src/utils/CompileHook.ts
@@ -0,0 +1,48 @@
+import { OhosHapContext, OhosPluginId } from '@ohos/hvigor-ohos-plugin';
+import { hvigor } from '@ohos/hvigor';
+
+/**
+ * 编译时 Hook 工具类
+ * 用于动态修改 module.json5 中的 metadata 配置
+ */
+export class CompileHook {
+  private static readonly KEY_GETUI_APPID = 'metadata_key';
+
+  /**
+   * 设置编译时的 metadata 修改 hook
+   * @param node 当前节点
+   */
+  public static setupMetadataHook(node: any): void {
+    node.afterNodeEvaluate((currentNode: any) => {
+      try {
+        const appContext = hvigor.getRootNode().getContext(OhosPluginId.OHOS_APP_PLUGIN) as any;
+        const buildMode = appContext.getBuildMode();
+
+        // 获取此节点使用插件的上下文对象 此时为hap插件 获取hap插件上下文对象
+        const hapContext = currentNode.getContext(OhosPluginId.OHOS_HAP_PLUGIN) as OhosHapContext;
+        // 通过上下文对象获取从module.json5文件中读出来的obj对象
+        const moduleJsonOpt = hapContext.getModuleJsonOpt();
+        
+        // 修改obj对象为想要的，此处修改module中的metadata
+        const originalMetadata = moduleJsonOpt['module']['metadata'];
+        console.log(`[八维通编译hook] 编译类型:${buildMode}, 原始metadata:${JSON.stringify(originalMetadata)}`);
+
+        // 查找并修改metadata_key的值
+        const metadataItem = moduleJsonOpt['module']['metadata'].find((item: any) => item.name === CompileHook.KEY_GETUI_APPID);
+        const debugMetaItem = moduleJsonOpt['module']['metadata'].find((item: any) => item.name === "debug_metadata_key");
+        if ((buildMode === 'debug' || buildMode === 'Default') && metadataItem && debugMetaItem) {
+          console.log(`[八维通编译hook] 找到metadata_key，原始值: ${metadataItem.value}, 修改为${debugMetaItem.value}`);
+          metadataItem.value = debugMetaItem.value;
+        } else {
+          console.log(`[八维通编译hook] 当前编译类型为release，或 没有检测到 metadata_key, 不修改, 编译类型: ${buildMode}`);
+        }
+        
+        console.log('[八维通编译hook] 修改后的metadata:', moduleJsonOpt['module']['metadata']);
+
+        hapContext.setModuleJsonOpt(moduleJsonOpt);
+      } catch (error) {
+        console.error('[八维通编译hook] 执行过程中发生错误:', error);
+      }
+    });
+  }
+}
